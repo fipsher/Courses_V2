18e21626116fbed5d98f8d937bd72d28b64dce6e
Andrew Zvarych <azvarych@exoft.net>
2017-04-03 12:55:58 +0300

    Add base commit

---
+++ Courses_v2/Courses_v2/Courses_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Courses_v2.Startup))]
+namespace Courses_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
