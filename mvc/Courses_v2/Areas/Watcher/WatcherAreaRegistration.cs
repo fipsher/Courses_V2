725fc7e7edda1405586d97bbe12c9c4e0c132e8e
Andrew Zvarych <azvarych@exoft.net>
2017-04-07 09:32:53 +0300

    Remove redundant code Add areas

---
+++ mvc/Courses_v2/Areas/Watcher/WatcherAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Courses_v2.Areas.Watcher
+{
+    public class WatcherAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Watcher";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Watcher_default",
+                "Watcher/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
