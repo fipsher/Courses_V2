990950ba22b8b9047ef09c786cae68cfb86ebebc
Andrii Zvarych Tarasovuch <fipsher@gmail.com>
2017-04-30 20:12:24 +0300

    Change views for Lecturer

--- mvc/Courses_v2/Areas/Moderator/Controllers/DisciplineController.cs
+++ mvc/Courses_v2/Areas/Moderator/Controllers/DisciplineController.cs
@@ -2,17 +2,23 @@
 using Core.Helpers;
 using Core.Interfaces.Services;
 using Courses_v2.Controllers;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using static Core.Enums.Enums;
 
 namespace Courses_v2.Areas.Moderator.Controllers
 {
-    // will be added some base discipline ctrl
     public class DisciplineController : BaseController<Discipline, IDisciplineService>
     {
-        public DisciplineController(IDisciplineService _disciplineService) : base(_disciplineService)
+        private readonly IUserService _userService;
+        private readonly ICathedraService _cathedraService;
+
+        public DisciplineController(IDisciplineService disciplineService, IUserService userService, ICathedraService cathedraService) : base(disciplineService)
         {
-            Service = _disciplineService;
+            Service = disciplineService;
+            _userService = userService;
+            _cathedraService = cathedraService;
         }
 
         // GET: Admin/Disciplines
@@ -26,30 +32,85 @@ namespace Courses_v2.Areas.Moderator.Controllers
         // GET: Admin/Disciplines/Details/5
         public ActionResult Details(string id)
         {
-            var discipline = Service.Find((new SearchFilter<Discipline>
+            var disciplines = Service.FindDisciplineResponse((new SearchFilter<Discipline>
             {
                 OptionList = new[] { new Discipline() { Id = id } }
-            })).SingleOrDefault();
-            if (CheckLecturerAccess(discipline))
+            }));
+
+            return View(disciplines.SingleOrDefault());
+        }
+
+        // GET: Admin/Disciplines/Create
+        public ActionResult Create()
+        {
+            ViewBag.Cathedras = _cathedraService.Find(SearchFilter<Cathedra>.Empty)
+                                                .Select(x => new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.Id
+                                                }); ;
+
+            var lecturersFilter = new SearchFilter<User>
+                                        {
+                                            OptionList = new List<User>()
+                                            {
+                                                new User(){Roles = new List<Role> { Role.Lecturer }}
+                                            }
+                                        };
+            ViewBag.Lecturers = _userService.Find(lecturersFilter)
+                                            .Select(x => new SelectListItem
+                                            {
+                                                Text = x.UserName,
+                                                Value = x.Id
+                                            });
+
+            return View();
+        }
+        // POST: Admin/Disciplines/Create
+        [HttpPost]
+        public ActionResult Create(Discipline discipline)
+        {
+            try
             {
-                return View(discipline);
+                if (ModelState.IsValid)
+                {
+                    Service.Add(discipline);
+                    return RedirectToAction("Index");
+                }
             }
-            return RedirectToAction("Index");// is better to redirect to Access denied 
+            catch
+            {
+                //
+            }
+            return View();
+
         }
 
         // GET: Admin/Disciplines/Edit/5
         public ActionResult Edit(string id)
         {
-            // same logic
-            var discipline = Service.Find(new SearchFilter<Discipline>()
+            var disciplines = Service.Find((new SearchFilter<Discipline>() { OptionList = new[] { new Discipline() { Id = id } } }));
+
+            ViewBag.Cathedras = _cathedraService.Find(SearchFilter<Cathedra>.Empty)
+                                                .Select(x => new SelectListItem
+                                                {
+                                                    Text = x.Name, Value = x.Id
+                                                }); ;
+
+            var lecturersFilter = new SearchFilter<User>
             {
-                OptionList = new[] { new Discipline() { Id = id } }
-            }).SingleOrDefault();
-            if (CheckLecturerAccess(discipline))
-            {
-                return View(discipline);
-            }
-            return RedirectToAction("Index");// is better to redirect to Access denied 
+                OptionList = new List<User>()
+                {
+                    new User(){Roles = new List<Role> { Role.Lecturer }}
+                }
+            };
+            ViewBag.Lecturers = _userService.Find(lecturersFilter)
+                                            .Select(x => new SelectListItem
+                                            {
+                                                Text = x.UserName, Value = x.Id
+                                            });
+
+            return View(disciplines.SingleOrDefault());
         }
         // POST: Admin/Disciplines/Edit/5
         [HttpPost]
@@ -57,13 +118,9 @@ namespace Courses_v2.Areas.Moderator.Controllers
         {
             try
             {
-                // lect logic
                 if (ModelState.IsValid)
                 {
-                    if (CheckLecturerAccess(discipline))
-                    {
-                        Service.Add(discipline);
-                    }
+                    Service.Add(discipline);
                     return RedirectToAction("Index");
                 }
             }
@@ -74,11 +131,29 @@ namespace Courses_v2.Areas.Moderator.Controllers
             return View();
         }
 
-        #region Helpers
-        private bool CheckLecturerAccess(Discipline discipline)
+        // GET: Admin/Disciplines/Delete/5
+        public ActionResult Delete(string id)
         {
-            return discipline.LecturerId == UserId;
+            var disciplines = Service.FindDisciplineResponse((new SearchFilter<Discipline>()
+            {
+                OptionList = new[] { new Discipline() { Id = id } }
+            }));
+
+            return View(disciplines?.SingleOrDefault());
         }
-        #endregion
+        // POST: Admin/Disciplines/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, Discipline discipline)
+        {
+            try
+            {
+                Service.Delete(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
