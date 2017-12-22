73046c0f92b60420806f02e87a6f404c7ee03092
Andruha4u <andrij4ik@gmail.com>
2017-12-22 19:26:16 +0200

    added start page

--- mvc/Courses_v2/Controllers/HomeController.cs
+++ mvc/Courses_v2/Controllers/HomeController.cs
@@ -8,15 +8,11 @@ namespace Courses_v2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
